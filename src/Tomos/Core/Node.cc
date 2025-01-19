282be142675d8a2938e9685e63e4ba8be3d7372e
oskar <oskar.suc@gmail.com>
2025-01-19 22:35:25 +0100

    added node class

--- src/Tomos/Core/Node.cc
+++ src/Tomos/Core/Node.cc
@@ -4,6 +4,37 @@
 
 #include "Node.hh"
 
+#include <functional>
+
 namespace Tomos
 {
+
+    Node::Node()
+    {
+        this->parent   = nullptr;
+        this->children = std::set<std::unique_ptr<Node>>();
+    }
+
+    void Node::addChild( std::unique_ptr<Node> child ) { this->children.insert( child ); }
+
+    bool Node::removeChild( std::unique_ptr<Node> child )
+    {
+        if ( this->children.erase( child ) )
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void Node::traverse( const std::function<void( Node& )>& before, const std::function<void( Node& )>& after )
+    {
+        before( *this );
+        for ( auto& current : children )
+        {
+            current->traverse( before, after );
+        }
+        after( *this );
+    }
+
+
 }  // namespace Tomos
--- src/Tomos/Core/Node.hh
+++ src/Tomos/Core/Node.hh
@@ -3,12 +3,25 @@
 //
 
 #pragma once
+#include <functional>
+#include <memory>
+#include <set>
 
 namespace Tomos
 {
 
     class Node
     {
+    public:
+        Node();
+        void addChild( std::unique_ptr<Node> child );
+        bool removeChild( std::unique_ptr<Node> child );
+        void traverse( const std::function<void( Node& )>& before, const std::function<void( Node& )>& after );
+        // TODO:
+
+    private:
+        std::unique_ptr<Node>           parent;
+        std::set<std::unique_ptr<Node>> children;
     };
 
 }  // namespace Tomos
